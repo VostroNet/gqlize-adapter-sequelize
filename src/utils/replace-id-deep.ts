80d6b42adb67b97db524ba3b4b6ef854aa850ac8
Matthew Mckenzie <mmckenzie@vostronet.com>
2022-10-05 00:34:55 +1000

    updating replace-id-deep

--- src/utils/replace-id-deep.ts
+++ src/utils/replace-id-deep.ts
@@ -7,40 +7,70 @@ function getProperties(obj: any): any {
   return [...Object.keys(obj), ...Object.getOwnPropertySymbols(obj)];
 }
 
+import { OKind, objVisit, BREAK } from "@vostro/object-visit";
 
-function checkObject(value: any, keyMap: any, variableValues: any, isTagged: boolean | undefined): any {
-  if (typeof value === "function") {
-    const result = value(variableValues);
-    return checkObject(result, keyMap, variableValues, isTagged);
-  } else if (Array.isArray(value)) {
-    return value.map((val) => {
-      return checkObject(val, keyMap, variableValues, isTagged);
-    });
-  } else if (Object.prototype.toString.call(value) === "[object Object]") {
-    return replaceIdDeep(value, keyMap, variableValues, isTagged);
-  } else if (isTagged) {
-    try {
-      return fromGlobalId(value).id;
-    } catch {
-      return value;
-    }
-  } else {
-    return value;
-  }
-}
-
-export default function replaceIdDeep(obj: any, keyMap: string[], variableValues: any, isTagged = false) {
+export default function replaceIdDeep(obj: any, keyMap: string[], variableValues: any) {
   if (obj instanceof Function) {
     obj = obj(variableValues);
   }
-  return getProperties(obj).reduce((m: any, key: string) => {
-    if (keyMap.indexOf(key) > -1 || isTagged) {
-      m[key] = checkObject(obj[key], keyMap, variableValues, true);
-    } else {
-      m[key] = checkObject(obj[key], keyMap, variableValues, false);
+  let tagged = false;
+  const result = objVisit(obj, {
+    [OKind.ARRAY]: {
+      enter(node, key, parent, path, ancestors) {
+        if(node instanceof Function) {
+          node = node(variableValues);
+        }
+        if(key && !tagged && keyMap.indexOf(`${key}`) > -1) {
+          tagged = true;          
+        }
+        return node;
+      },
+      leave(node, key, parent, path, ancestors) {
+        if (tagged && keyMap.indexOf(`${key}`) > -1) {
+          tagged = false;
+        } 
+        return node;
+      }
+    },
+    [OKind.OBJECT]: {
+      enter(node, key, parent, path, ancestors) {
+        if(node instanceof Function) {
+          node = node(variableValues);
+        }
+        if(key && !tagged && keyMap.indexOf(`${key}`) > -1) {
+          tagged = true;          
+        }
+        return node;
+      },
+      leave(node, key, parent, path, ancestors) {
+        if (tagged && keyMap.indexOf(`${key}`) > -1) {
+          tagged = false;
+        } 
+        return node;
+      }
+    },
+    [OKind.FIELD]: {
+      enter(node, key, parent, path, ancestors) {
+        if (node instanceof Function) {
+          node = node(variableValues);
+        }
+        if (key && !tagged && keyMap.indexOf(`${key}`) > -1) {
+          tagged = true;          
+        }
+        if (tagged && typeof node === "string" && /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/.test(node)) {
+          return fromGlobalId(node).id;
+        }
+        return node;
+      },
+      leave(node, key, parent, path, ancestors) {
+        if (tagged && keyMap.indexOf(`${key}`) > -1) {
+          tagged = false;
+        } 
+        return node;
+      }
     }
-    return m;
-  }, {} as any);
+  });
+  return result;
 }
 
 
