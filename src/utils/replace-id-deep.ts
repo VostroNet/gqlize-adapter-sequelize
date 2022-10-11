34545923b939be71c32f3049501b8ed341b68ce0
Matthew Mckenzie <mmckenzie@vostronet.com>
2022-10-11 15:52:09 +1000

    fix for uploads

--- src/utils/replace-id-deep.ts
+++ src/utils/replace-id-deep.ts
@@ -17,9 +17,9 @@ export default function replaceIdDeep(obj: any, keyMap: string[], variableValues
   const result = objVisit(obj, {
     [OKind.ARRAY]: {
       enter(node, key, parent, path, ancestors) {
-        if(node instanceof Function) {
-          node = node(variableValues);
-        }
+        // if(node instanceof Function) {
+        //   node = node(variableValues);
+        // }
         if(key && !tagged && keyMap.indexOf(`${key}`) > -1) {
           tagged = true;          
         }
@@ -34,9 +34,9 @@ export default function replaceIdDeep(obj: any, keyMap: string[], variableValues
     },
     [OKind.OBJECT]: {
       enter(node, key, parent, path, ancestors) {
-        if(node instanceof Function) {
-          node = node(variableValues);
-        }
+        // if(node instanceof Function) {
+        //   node = node(variableValues);
+        // }
         if(key && !tagged && keyMap.indexOf(`${key}`) > -1) {
           tagged = true;          
         }
@@ -51,9 +51,9 @@ export default function replaceIdDeep(obj: any, keyMap: string[], variableValues
     },
     [OKind.FIELD]: {
       enter(node, key, parent, path, ancestors) {
-        if (node instanceof Function) {
-          node = node(variableValues);
-        }
+        // if (node instanceof Function) {
+        //   node = node(variableValues);
+        // }
         if (key && !tagged && keyMap.indexOf(`${key}`) > -1) {
           tagged = true;          
         }
